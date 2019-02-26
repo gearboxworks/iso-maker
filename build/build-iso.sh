96b42199a9183895dd683cbcb33d6e5011306951
MickMake <mick@mickmake.com>
2019-02-27 10:41:07 +1100

    Added tarball checks.

--- build/build-iso.sh
+++ build/build-iso.sh
@@ -1,29 +1,50 @@
-#!/bin/bash
+#!/bin/bash -l
 
-echo "Setting up rootfs..."
+# We need to use the '-l' option for bash to ensure we pull in all the profile setup.
+# As part of the profile login, we execute /etc/profile.d/rootfs.sh, which untars the rootfs.changes.tar.gz file into /tmp/rootfs/
+
+# set -x
+
+echo "# Setting up rootfs..."
 cd /build
 cp /build/genapkovl-*.sh /build/mkimg.*.sh /build/aports/scripts
 rm -rf /tmp/rootfs/opt/gearbox-fallback
 git clone -b 0.5.0 https://github.com/gearboxworks/box-scripts /tmp/rootfs/opt/gearbox-fallback
 
-tar zcf /build/rootfs.changes.tar.gz -C /tmp/rootfs .
+if [ -d /tmp/rootfs ]
+then
+	echo "# Tarballing /tmp/rootfs to /build/rootfs.changes.tar.gz"
+	tar zcf /build/rootfs.changes.tar.gz -C /tmp/rootfs .
+fi
 
-echo "Building..."
+echo "# Building..."
 cd /build/aports/scripts
-./mkimage.sh --tag 1.0 \
+./mkimage.sh --tag 0.5.0 \
 	--outdir /build/iso \
 	--arch x86_64 \
 	--repository https://mirror.aarnet.edu.au/pub/alpine/v3.8/main \
 	--extra-repository https://mirror.aarnet.edu.au/pub/alpine/v3.8/community \
 	--profile gearbox_small >& /build/iso/output.log
 
-echo "Completed."
+echo "# Completed."
 
 echo ""
-echo ""
+echo "# Tail build/iso/output.log"
 tail /build/iso/output.log
 
 echo ""
-echo ""
-ls -lrth /build/iso/*.iso
+echo "# Show ISOs"
+MD5SUM="$(which md5sum)"
+for iso in /build/iso/*.iso
+do
+	echo "# ${iso}"
+	if [ "${MD5SUM}" != "" ]
+	then
+		$MD5SUM "${iso}" > "${iso}.md5sum"
+		ls -lh "${iso}" "${iso}.md5sum" 2>/dev/null
+		cat "${iso}.md5sum" 2>/dev/null
+	else
+		ls -lh "${iso}"
+	fi
+done
 
